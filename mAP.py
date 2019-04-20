d83d881d083f474045b2d5d35321ff539500993d
legendchilli <37359271+suyanzhou626@users.noreply.github.com>
2019-04-20 13:31:05 +0800

    Update mAP.py

--- mAP.py
+++ mAP.py
@@ -20,3 +20,30 @@ def calmAP(output, target):
                 apsum = apsum + right/(i+1)
         ap[k] = apsum/sum(targetk)
     return mAP
+
+def calcAP(output, target, w=4):
+    '''
+    Args:
+        output: preds of model softmax need to be used.
+        target: gt
+        w: weight
+    Return:
+        cAP: 
+    '''
+    ap = np.zeros(output.shape[1])
+    for k in range(output.shape[1]):
+        outputk = output[:, k]
+        targetk = target[:, k] * w
+    #     print(targetk)
+        sortind = np.argsort(-outputk, axis=0)
+        apsum = 0.0
+        right = 0.0
+        for i in range(len(sortind)):
+            idx = sortind[i]
+            if targetk[idx] == w:
+                right = right + 1*w
+                addw = lambda w: (w-1)*right/w if w!=1 else 0
+                apsum = apsum + right/(i+1+ addw(w))
+        ap[k] = apsum/sum(targetk)
+        
+    return ap.mean()
